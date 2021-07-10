93539335b98d95f1959a24539be57648efc2e491
Israel Adefidipe <israeladefidipe@gmail.com>
2021-07-10 22:27:54 +0100

    hamburger animation and dropdown functionality done

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,24 @@
 'use strict'
 
+//  DOM elements
+const btnHamburger = document.querySelector('#btnHamburger');
+const overlay = document.querySelector('.header__overlay');
+const headerMenu = document.querySelector ('.header__menu');
+const header = document.querySelector('header');
+const headerLinks = document.querySelector(".header__links");
 
+
+//* create mobile dropdown functionality
+btnHamburger.addEventListener('click', function(e){
+  console.log(headerLinks);
+  btnHamburger.classList.toggle("hamburger");
+  overlay.classList.toggle('overlay');
+  headerLinks.classList.toggle('drop-down');
+})
+
+
+
+// Swiper JS initialized
 var swiper = new Swiper(".swiper-container", {
   spaceBetween: 30,
   centeredSlides: true,
@@ -17,3 +35,4 @@ var swiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
