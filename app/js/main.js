7a48762040240d68d92489a8ac8c5f407a32f33d
Israel Adefidipe <israeladefidipe@gmail.com>
2021-07-12 04:09:13 +0100

    JS === Implemented email validation

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,39 @@ const overlay = document.querySelector('.header__overlay');
 const headerMenu = document.querySelector ('.header__menu');
 const header = document.querySelector('header');
 const headerLinks = document.querySelector(".header__links");
+const email = document.querySelector('#email');
+const errorAlert = document.querySelector('.error-alert')
+const emailBtn = document.querySelector(".footer__contact-cta");
+const form = document.querySelector(".footer__contact-form");
+
+
+// * email validation
+
+let regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+function check(){
+  if (email.value.match(regExp)){
+    email.style.border = "2px solid green";
+    errorAlert.style.display = "none";
+    emailBtn.style.display = "block";
+    email.style.backgroundColor = "rgb(242, 255, 239)";
+
+  }else if(email.value === ""){
+    email.style.border = "none";
+    errorAlert.style.display = "none";
+    email.style.backgroundColor = "white";
+    emailBtn.style.display = "none";
+  }
+  else{
+    
+    email.style.border = "2px solid red";
+    email.style.backgroundColor = "rgb(255, 239, 239)";
+    errorAlert.style.display = 'block';
+    emailBtn.style.display = 'none'
+  }
+}
+
+
 
 
 //* create mobile dropdown functionality
